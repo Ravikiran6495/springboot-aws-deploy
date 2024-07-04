a3fd29bc85d07a0433129c23d30257a528243f87
ravikiran6495 <takalerm9@gmail.com>
2024-07-04 12:30:13 +0530

    first-commit

---
+++ src/main/java/com/lti/awsdemo/controller/StudentsController.java
@@ -0,0 +1,15 @@
+package com.lti.awsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentsController {
+
+
+    @GetMapping("welcome")
+    public  String calling(){
+        return "aws demo";
+    }
+
+}
